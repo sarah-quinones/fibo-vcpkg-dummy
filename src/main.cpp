c700bf33d42dc1adfd32d8486df3b0464b4bc4b2
kitegi <>
2021-05-06 03:37:32 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <cxxopts.hpp>
+#include <fmt/format.h>
+#include <range/v3/view.hpp>
+
+namespace view = ranges::views;
+
+int fib(int x) {
+  int a = 0, b = 1;
+
+  for (int it : view::repeat(0) | view::take(x)) {
+    (void)it;
+    int tmp = a;
+    a += b;
+    b = tmp;
+  }
+
+  return a;
+}
